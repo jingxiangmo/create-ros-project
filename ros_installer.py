ef2348f43ba0118a299b72c2db210cdc17c33a55
Jingxiang Mo <jingxiangmo@gmail.com>
2024-02-17 13:44:10 +0800

    ROS installer

--- ros_installer.py
+++ ros_installer.py
@@ -1,11 +1,59 @@
 from utils import *
 
+"""
+NOTE: Installation scripts are provided by the official documentation. For safety reasons, the commands should NOT be  
+different from the official documentation. 
+If you see any mistakes or differences. Please open a Github Issue immediately.
+
+ROS 1:
+Noetic Ninjemys: http://wiki.ros.org/noetic/Installation/Ubuntu
+Foxy Fitzroy: https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+
+ROS 2:
+Iron Irwini:https://docs.ros.org/en/iron/Installation/Ubuntu-Install-Debians.html
+Foxy Fitzroy: https://docs.ros.org/en/foxy/Installation/Alternatives/Ubuntu-Development-Setup.html
+"""
+
 
 def run_ros_install(ros_version, ros_distribution):
+    distributions = {
+        "ROS 1 Noetic": "noetic",
+        "ROS 1 Melodic": "melodic",
+
+        "ROS 2 Iron": "iron",
+        "ROS 2 Foxy": "foxy"
+    }
+
+    ros_distribution = distributions[ros_distribution]
+
     if ros_version == "1":
         print("Installing ROS1...")
-        # run(["sudo", "apt-get", "update"])
-        # run(["sudo", "apt-get", "install", "-y", "ros-noetic-desktop-full"])
+
+        # setup sources.list
+        run_command(
+            """sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' """)
+
+        # set up keys
+        run_command("sudo apt install curl")
+        run_command("curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -")
+
+        # installation
+        run_command("sudo apt update")
+        run_command("sudo apt install ros-" + ros_distribution + "-desktop-full")
+
+        # environment setup
+        run_command("source /opt/ros/" + ros_distribution + "/setup.bash >> ~/.bashrc")
+        run_command("source ~/.bashrc")
+
+        # dependencies for building packages
+        run_command(
+            "sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential")
+
+        # initialize rosdep
+        run_command("sudo apt install python3-rosdep")
+        run_command("sudo rosdep init")
+        run_command("rosdep update")
+
 
     elif ros_version == "2":
         print("Installing ROS2...")
@@ -38,7 +86,10 @@ def run_ros_install(ros_version, ros_distribution):
         # install ros2 packages
         run_command("sudo apt update")
         run_command("sudo apt upgrade")
-        run_command("sudo apt install ros-" + ros_distribution + "python3-argcomplete")
+        run_command("sudo apt install ros-" + ros_distribution + " python3-argcomplete")
         run_command("sudo apt install ros-dev-tools")
+
+        # environment setup
+        run_command("source /opt/ros/" + ros_distribution + "/setup.bash")
 
     print("ROS installation complete.")
